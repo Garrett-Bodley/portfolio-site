b8ea5103138c0d036f59f0cc6234003d65fb9967
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-09 12:46:00 -0500

    Create SVG down icon component.

---
+++ src/icons/Down.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Down = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 100 125"
+      enableBackground="new 0 0 100 100"
+      xmlSpace="preserve"
+    >
+      <polygon points="50,72.5 5,27.5 22,27.5 50,55.5 78,27.5 95,27.5 " />
+      {/* <text
+        x="0"
+        y="115"
+        fill="#000000"
+        fontSize="5px"
+        fontWeight="bold"
+        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        Created by James Donnelly
+      </text>
+      <text
+        x="0"
+        y="120"
+        fill="#000000"
+        fontSize="5px"
+        fontWeight="bold"
+        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        from the Noun Project
+      </text> */}
+    </svg>
+  );
+};
+
+export default Down;
