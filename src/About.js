f341dcbce769e5a309f360e810ac92da42d96998
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-09 12:44:45 -0500

    Build out an About Me component.

---
+++ src/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./about.module.css";
+
+const About = () => {
+  return (
+    <div className={style.container}>
+      <section className={style.card}>
+        <h1 className={style.title}>About Me</h1>
+        <p className={style.content}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
