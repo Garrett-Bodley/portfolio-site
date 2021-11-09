933fbc6515728f4875200ac716ff60d2d6128742
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-09 16:51:58 -0500

    Implement useScrollPosition hook.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import React from "react";
 import LandingPage from "./LandingPage";
 import About from "./About";
 import Portfolio from "./Portfolio";
+import Noodlr from "./Noodlr";
+import Breadbook from "./Breadbook";
+import { useScrollPosition } from "./utilities/useScrollPosition"
 
 function App() {
+
+  useScrollPosition('scrollY')
   return (
     <div className="App">
       <LandingPage />
       <About />
       <Portfolio />
+      <Noodlr />
+      <Breadbook />
     </div>
   );
 }
