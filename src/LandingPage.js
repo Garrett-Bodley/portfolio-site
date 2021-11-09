54d31daff49b2da730e92e46225eb2f5c5da8392
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-09 12:44:32 -0500

    Build out a landing page component.

---
+++ src/LandingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from  "./landingPage.module.css";
+import Down from "./icons/Down"
+
+const LandingPage = () => {
+  return (
+    <div className={styles.landingPage}>
+      <h1 className={styles.title}>Garrett Bodley</h1>
+      <p className={styles.tagline}>a nyc based web developer</p>
+      {/* <p className={styles.tagline}>Problem Solver</p> */}
+      {/* <Divider/> */}
+      <Down className={styles.icon} />
+    </div>
+  );
+};
+
+export default LandingPage;
