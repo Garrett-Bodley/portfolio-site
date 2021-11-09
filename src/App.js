1aacbe2b6245e981086424c78e12460dd466e526
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-09 12:47:30 -0500

    Import and render new components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import LandingPage from "./LandingPage";
+import About from "./About";
+import Portfolio from "./Portfolio";
 
 function App() {
   return (
     <div className="App">
+      <LandingPage />
+      <About />
+      <Portfolio />
     </div>
   );
 }
