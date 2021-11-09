5cf0e2db191c4f448fe225bdd3b9894c2c27a5c7
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-09 14:04:04 -0500

    Change portfolio component into a hero.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,12 +5,7 @@ import Dots from './icons/Dots'
 const Portfolio = () => {
   return (
     <div className={style.container}>
-      <section className={style.card}>
-        <article className={`${style.project} ${style.projectOne}`}></article>
-        <article className={`${style.project} ${style.projectTwo}`}></article>
-        <article className={`${style.project} ${style.projectThree}`}></article>
-        <Dots count={3} current={1} styles={style.dots} />
-      </section>
+      <h1 className={style.title} >Projects</h1>
     </div>
   )
 }
