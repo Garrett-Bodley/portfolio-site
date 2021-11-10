f2485f1c4370f7c0a75649e71c04af748cf8559a
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-10 15:44:45 -0500

    Add SVG icons.

---
+++ src/icons/Logo.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Logo = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 100 100"
+      enableBackground="new 0 0 100 100"
+      xmlSpace="preserve"
+    >
+      <path d="M7.2,84.7h85.5c3.2,0,5.8-2.6,5.8-5.8v-2.5c0-0.5-0.2-1-0.5-1.4l-4.3-5.3c-0.7-0.9-1.1-1.9-1.1-3v-5v-3.2v-9.2l0,0V23.7  c0-4.7-3.8-8.5-8.5-8.5H52.4H15.9c-4.7,0-8.5,3.8-8.5,8.5v34.8v1.9v6.2c0,1.1-0.4,2.2-1.1,3L2,75.1c-0.3,0.4-0.5,0.9-0.5,1.4V79  C1.5,82.2,4.1,84.7,7.2,84.7z M59.2,76.4c0,1.6-1.3,2.9-2.9,2.9H43.8c-1.6,0-2.9-1.3-2.9-2.9v-0.6h18.4V76.4z M15.9,60.8v-2.2V23.7  c0,0,0,0,0,0h40.3h27.9c0,0,0,0,0,0v34.9v2.2v0.6h0v0.4v5.5H15.9V60.8z M54.9,32.9l3.9,2.2l-13.7,24l-3.9-2.2L54.9,32.9z M38.5,38  l-8,8l8,8l-3.2,3.2l-8-8L24.2,46l3.2-3.2l8-8L38.5,38z M72.6,42.8l3.2,3.2l-3.2,3.2l-8,8L61.5,54l8-8l-8-8l3.2-3.2L72.6,42.8z" />
+      {/* <text
+        x="0"
+        y="115"
+        fill="#000000"
+        fontSize="5px"
+        fontWeight="bold"
+        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        Created by Gregor Cresnar
+      </text>
+      <text
+        x="0"
+        y="120"
+        fill="#000000"
+        fontSize="5px"
+        fontWeight="bold"
+        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        from the Noun Project
+      </text> */}
+    </svg>
+  );
+};
+
+export default Logo;
