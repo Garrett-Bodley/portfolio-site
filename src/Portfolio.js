806464e02525360e5a1be80e11391df22c863778
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-10 15:45:55 -0500

    Add Down icon.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import style from './portfolio.module.css'
-import Dots from './icons/Dots'
+import Down from './icons/Down'
 
 const Portfolio = () => {
   return (
     <div className={style.container}>
       <h1 className={style.title} >Projects</h1>
+      <Down className={style.icon} />
     </div>
   )
 }
