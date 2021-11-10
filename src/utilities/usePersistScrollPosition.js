5a061e96fbb6600b4b466e33ca4c3dd8dda527cd
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-10 11:19:48 -0500

    Rename custom hook to persist scrolling position.

--- src/utilities/usePersistScrollPosition.js
+++ src/utilities/usePersistScrollPosition.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 import useLocalStorage from './useLocalStorage'
 
-export const useScrollPosition = (key) => {
+export const usePersistScrollPosition = (key) => {
   const [position, setPosition] = useLocalStorage(key, 0)
   useEffect(() => {
     window.scrollTo(0, position);
     return () => {setPosition(window.scrollY)}
   }, [])
+  return position
 }
