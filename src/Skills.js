4eab3362901aeae8875b07e5c564fba78f4178af
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-14 14:07:52 -0500

    Build out Skills component.

--- src/Skills.js
+++ src/Skills.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import styles from './skills.module.css'
+import { Css, Git, Html, Postgres, Rails, ReactIcon, Redux, Sqlite} from "./icons/SkillsIcons"
 
 const Skills = () => {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.iconWrapper}>
+        <Css className={styles.icon} />
+        <Git className={styles.icon} />
+        <Html className={styles.icon} />
+        <Postgres className={styles.icon} />
+      </div>
+      <div className={styles.iconWrapper}>
+        <Rails  className={styles.icon} />
+        <ReactIcon className={styles.icon} />
+        <Redux className={styles.icon} />
+        <Sqlite className={styles.icon} />
+      </div>
     </div>
   )
 }
