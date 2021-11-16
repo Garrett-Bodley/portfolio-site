fa526c0e886ad1a7b81933811393e949228bed0f
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-16 14:16:31 -0500

    Add actual content to About card.

--- src/About.js
+++ src/About.js
@@ -8,13 +8,13 @@ const About = () => {
     <div id="about" className={style.container}>
         <h1 className={style.title}>About Me <Bio className={style.icon} /></h1>
         <p className={style.content}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Former musician turned web developer.
+        </p>
+        <p className={style.content}>
+          In many ways, I think the aspects of my personality that made me excel musically are the same that drove me towards the world of programming. A huge part of studying music is the process of exploring new systems, learning the various rules, and seeing how they relate to what you already know. I've found programming to be remarkably similar, with the many frameworks and languages akin to different scales, chords, songs, or ensembles.
+        </p>
+        <p className={style.content}>
+          I strongly believe that my creative, curious demeanor, and ability to flexibly integrate into a team is of value to any future employer.
         </p>
         <Skills/>
     </div>
