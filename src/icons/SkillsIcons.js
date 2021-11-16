a400219b13bdef39bf8fe1226ecbe79c7bc15eee
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-16 14:01:57 -0500

    Remove inline height/width from HerokuIcon.

--- src/icons/SkillsIcons.js
+++ src/icons/SkillsIcons.js
@@ -41,8 +41,6 @@ export const HerokuIcon = (props) => {
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24px"
-      height="24px"
       viewBox="0 0 24 24"
       role="img"
     >
