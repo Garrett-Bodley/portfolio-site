97b99704757228eda24147126c264125783ae944
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-16 13:55:42 -0500

    Create Github and Link icons.

---
+++ src/icons/LinkIcon.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const LinkIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 100"
+      x="0px"
+      y="0px"
+    >
+      <title>Link</title>
+      <path d="M12.53613,87.47586a25.72756,25.72756,0,0,1,0-36.38567l3.9878-3.98956a4.00056,4.00056,0,0,1,5.65722,5.65807l-3.98828,3.98955A17.724,17.724,0,1,0,43.25439,81.8178L56.43506,68.63292a17.74172,17.74172,0,0,0-3.90918-28.03827,4.0009,4.0009,0,0,1,3.88672-6.99443A25.74591,25.74591,0,0,1,62.09229,74.291L48.91113,87.47586a25.74424,25.74424,0,0,1-36.375,0ZM51.08887,12.52414,37.90771,25.709A25.745,25.745,0,0,0,43.5874,66.39978a4.0009,4.0009,0,0,0,3.88672-6.99443,17.7423,17.7423,0,0,1-3.90918-28.03827L56.74561,18.1822A17.724,17.724,0,0,1,81.80713,43.25175L77.81885,47.2413a4.00056,4.00056,0,1,0,5.65722,5.65807l3.9878-3.98956a25.72478,25.72478,0,0,0-36.375-36.38567Z" />
+      {/* <text
+        x="0"
+        y="115"
+        fill="#000000"
+        font-size="5px"
+        font-weight="bold"
+        font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        Created by TTHNga
+      </text>
+      <text
+        x="0"
+        y="120"
+        fill="#000000"
+        font-size="5px"
+        font-weight="bold"
+        font-family="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      >
+        from the Noun Project
+      </text> */}
+    </svg>
+  );
+};
+
+export default LinkIcon;
