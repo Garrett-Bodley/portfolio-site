cba8180217176a741037a7d12efae502a97a2f45
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-23 14:47:09 -0500

    Swap lorem ipsum for actual content.

--- src/Noodlr.js
+++ src/Noodlr.js
@@ -6,16 +6,16 @@ const Noodlr = () => {
   return (
     <div className={styles.container}>
       <span className={styles.titleWrapper}>
-        <h1 className={styles.title}>Noodlr<Sequencer className={styles.icon} /></h1>
+        <h1 className={styles.title}>Noodlr 2.0<Sequencer className={styles.icon} /></h1>
       </span>
-      <p className={styles.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+      <p className={styles.content}>
+        TLDR: Noodlr with a facelift.
+      </p>
+      <p className={styles.content}>
+        While I was happy with the logic of the original Noodlr, the site's UI was a bit underwhelming. After spending some time learning more about CSS and front end technologies in general, I decided to rebuild it from scratch.
+      </p>
+      <p className={styles.content}>
+        Noodlr 2.0 is built with 100% functional components. It utilizes custom hooks, custom Context Providers, react-spring and more. Currently a front-end only web-app, though a fleshed out back end is in the works.
       </p>
     </div>
   );
