9b6af6bf7a45cadd5ce72af801cc3dd1c581cae8
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-23 14:47:16 -0500

    Tweak bio text.

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <div id="about" className={style.container}>
         <h1 className={style.title}>About Me <Bio className={style.icon} /></h1>
         <p className={style.content}>
-          Former musician turned web developer.
+          A former musician turned web developer.
         </p>
         <p className={style.content}>
           In many ways, I think the aspects of my personality that made me excel musically are the same that drove me towards the world of programming. A huge part of studying music is the process of exploring new systems, learning the various rules, and seeing how they relate to what you already know. I've found programming to be remarkably similar, with the many frameworks and languages akin to different scales, chords, songs, or ensembles.
