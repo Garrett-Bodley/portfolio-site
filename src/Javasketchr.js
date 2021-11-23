a846fe6ff10ad75dd90d6d8c607bf548bbd14d7f
Garrett-Bodley <garrett.bodley@gmail.com>
2021-11-23 14:47:35 -0500

    Swap lorem ipsum for actual content.

--- src/Javasketchr.js
+++ src/Javasketchr.js
@@ -1,19 +1,16 @@
 import React from "react";
-import style from "./javasketchr.module.css";
+import styles from "./javasketchr.module.css";
 import Drawing from "./icons/Drawing";
 
 const Javasketchr = () => {
   return (
-    <div className={style.container}>
-      <h1 className={style.title}>Javasketchr <Drawing className={style.icon}/></h1>
-      <p className={style.description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+    <div className={styles.container}>
+      <h1 className={styles.title}>Javasketchr <Drawing className={styles.icon}/></h1>
+      <p className={styles.content}>
+        A fun single page application that lets users express themselves creatively through drawing. Drawings can be saved locally and/or uploaded to the server.
+      </p>
+      <p className={styles.content}>
+        HTML and Javascript front end which communicates with a Rails API backaned via asynchronous fetch requests.
       </p>
     </div>
   );
